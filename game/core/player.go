85b16b429d76ca8d11e7b6958c146318423f2be5
MeenAnusak <meenanusak@gmail.com>
2024-02-14 23:52:43 +0700

    .

--- game/core/player.go
+++ game/core/player.go
@@ -8,6 +8,6 @@ import (
 type Player struct {
 	PlayerID   uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4();" json:"player_id"`
 	PlayerName string    `json:"player_name"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	CreatedAt  time.Time `gorm:"autoTimeCreate;" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoTimeUpdate;" json:"updated_at"`
 }
--- game/handler/player_handler.go
+++ game/handler/player_handler.go
@@ -14,11 +14,11 @@ func NewHttpPlayerHandler(service *core.PlayerServiceImpl) *HttpPlayerHandler {
 }
 
 func (h *HttpPlayerHandler) GetAllPlayer(ctx *fiber.Ctx) error {
-	player, err := h.service.GetAllPlayer()
+	_, err := h.service.GetAllPlayer()
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
-	return ctx.Status(fiber.StatusOK).JSON(player)
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Create New Player Success"})
 
 }
 
